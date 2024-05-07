a499b1de0e5822922c529cc6dd8220b3b69a5f6f
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-08 01:59:20 +0700

    add:required

--- src/routes/control/+page.svelte
+++ src/routes/control/+page.svelte
@@ -13,9 +13,9 @@
 
 <form class="my-5 w-full lg:w-1/2 text-xl mx-auto text-center bg-gray-300 rounded-lg py-4" id="myForm" method="POST" action="?/add">
 	<h1 class="text-center text-4xl ">Aisha Donate API Editer</h1>
-	<p class="inline"> รับมา(เฉพาะยอดใหม่): <input class="w-auto inline p-1  text-sm text-gray-900 bg-gray-400 rounded-lg border border-gray-300" type="number" step="0.01" name="Recived" value="">บาท</p><br>
-	<p class="inline">ยอดทั้งหมด(ไม่ต้องแก้): <input class="w-auto w-fit p-1  text-sm text-gray-900 bg-gray-400 rounded-lg border border-gray-300" type="number" step="0.01" name="Needed" value=20000>บาท</p><br>
-	Authentication Code: <input class=" p-1  text-sm text-gray-900 bg-gray-400 rounded-lg border border-gray-300" type="password" name="Auth" value=""><br>
+	<p class="inline"> รับมา(เฉพาะยอดใหม่(ทศนิยม2ตำแหน่ง)): <input class="w-auto inline p-1  text-sm text-gray-900 bg-gray-400 rounded-lg border border-gray-300" type="number" step="0.01" name="Recived" value="" required>บาท</p><br>
+	<p class="inline">ยอดทั้งหมด(ไม่ต้องแก้): <input class="w-auto w-fit p-1  text-sm text-gray-900 bg-gray-400 rounded-lg border border-gray-300" type="number" step="0.01" name="Needed" value=20000 required>บาท</p><br>
+	Authentication Code: <input class=" p-1  text-sm text-gray-900 bg-gray-400 rounded-lg border border-gray-300" type="password" name="Auth required" value=""><br>
 		{#if form?.auth === false}
 			<p class="text-red-500">Authentication Code ไม่ถูกต้อง</p>
 		{:else if form?.auth === true && form?.server === false}
