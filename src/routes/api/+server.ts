5aee5ba86ec67ab8f72087e9908718efcba4ef2b
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-08 15:07:34 +0700

    rate:limit

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,7 +1,22 @@
 import { db } from '$lib/db.server';
+import { RateLimiter } from 'sveltekit-rate-limiter/server';
+import { error } from '@sveltejs/kit';
+
+const limiter = new RateLimiter({
+	IP: [30, 's'], // IP address limiter
+	IPUA: [30, 's'], // IP + User Agent limiter
+	cookie: {
+		// Cookie limiter
+		name: 'limiterid', // Unique cookie name for this limiter
+		secret: 'SECRETKEY-SERVER-ONLY', // Use $env/static/private
+		rate: [5, 's'],
+		preflight: false // Require preflight call (see load function)
+	}
+});
 
 // eslint-disable-next-line no-empty-pattern
-export const GET = async ({}) => {
+export const GET = async (event) => {
+	if (await limiter.isLimited(event)) error(429);
 	const VerifyDonateData = await db.donateData.findFirst({
 		where: {
 			id: "1",
