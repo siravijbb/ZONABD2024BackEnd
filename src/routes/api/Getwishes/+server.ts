21e650444d7641c80cfc59d4b9faa5e5ceec4fe4
Siravij Praevisavakij <siravijbb@gmail.com>
2024-05-25 20:12:32 +0700

    idk

--- src/routes/api/Getwishes/+server.ts
+++ src/routes/api/Getwishes/+server.ts
@@ -119,7 +119,7 @@ export const GET = async (event) => {
 					}
 				})
 			);
-		} else if (currentDateTimeUTC.getTime() > predefinedDateTimeObject.getTime()) {
+		} else if (currentDateTimeUTC.getTime() > predefinedDateTimeObject.getTime() && !OverideFormAccepting) {
 			FormAccepting = false;
 			Readable = true;
 			let count = await db.wishes.count();
@@ -140,7 +140,7 @@ export const GET = async (event) => {
 				})
 			);
 		}
-		else if (currentDateTimeUTC.getTime() > predefinedDateTimeObject.getTime() || OverideFormAccepting == true) {
+		else if (currentDateTimeUTC.getTime() > predefinedDateTimeObject.getTime() && OverideFormAccepting == true) {
 			FormAccepting = true;
 			Readable = true;
 			let count = await db.wishes.count();
